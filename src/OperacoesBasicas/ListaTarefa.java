5aded5a7379eed2bb0646ce0e63a23e68ddf388a
luscasrocha <l.vicente@live.com>
2024-05-26 18:29:53 -0300

    Resolvendo desafio ListaTerafa
    Signed-off-by: luscasrocha <l.vicente@live.com>

---
+++ src/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,62 @@
+package OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    //adicionando tarefa na lista
+     public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+     }
+
+     //removendo as tarefas da lista com essa descrição
+     public void removerTarefa(String descricao) {
+        //criando uma nova lista para inserir nela as tarefas que vai remover
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList) {
+            if(t.getDescricao().equalsIgnoreCase(descricao))
+            {
+                tarefasParaRemover.add(t);
+            }
+        }
+        //usar o removeAll ao invés do Remove pois se tiver mais de uma descrição com o mesmo nome não iria remover, apenas a primeira
+        tarefaList.removeAll(tarefasParaRemover);
+     }
+
+     //descobrindo a quantidade de elementos que tem dentro da lista
+     public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+     }
+
+     //imprimindo a lista de tarefas
+     public void obterDescricoesTarefas(){
+         System.out.println(tarefaList);
+     }
+
+    public static void main(String[] args) {
+        //criando objeto
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        System.out.println("O nº total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        //testando add
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        System.out.println("O nº total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        //testando remover
+        listaTarefa.removerTarefa("Tarefa 1");
+        System.out.println("O nº total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        //testando listagem
+        listaTarefa.obterDescricoesTarefas();
+    }
+
+}
